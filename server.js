015569c0e0337fb50714846b062f83d8fa040e7c
cogicgoran <cogicgoran99@gmail.com>
2025-01-09 15:45:57 +0100

    Added some interaction

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 import http from 'http';
 import fs from "fs";
 
+const articles = ['Gogaric'];
+
+function getListOfArticles(){
+    const content = articles.map((item) => `<li>${item}</li>`).join('\n')
+    console.log('content:', content)
+    return content
+}
+
 const server = http.createServer((req,res) => {
     console.log(`Received request, URL: ${req.url}`)
-    if (req.url === '/') {
-        res.write(fs.readFileSync('index.html'));
+    if (req.url === '/' && req.method === 'GET') {
+        res.setHeader('Content-Type', 'text/html; charset=UTF-8')
+        res.write(fs.readFileSync('index.html','utf-8'), 'utf-8');
         res.statusCode = 200;
         res.end()
         return;
     }
-    if (req.url === '/articles') {
-        res.write('<ul><li>Gogaric</li></ul>');
+    if (req.url === '/articles' && req.method === "GET") {
+        res.setHeader('Content-Type', 'text/html; charset=UTF-8')
+        res.write(`<ul>${getListOfArticles()}</ul>`, 'utf-8');
         res.statusCode = 200;
+        res.write
         res.end()
         return;
+    }
+    if (req.url === '/articles' && req.method === "POST") {
+        req.setEncoding('utf-8')
+        req.on('data',(data) => {
+            const item = data.split('=')[1]
+            articles.push(item);
+            console.log(`Added item: '${item}'`)
+            console.log(articles);
+            res.statusCode = 201;
+            res.end()
+            return;
+        })
+        return
     }
     res.statusCode = 404
     res.end()
